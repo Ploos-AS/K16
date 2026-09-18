e2b13a73330780e91af7e588e9ae513c6c7eb8cd
pos <pgousdal@users.noreply.github.com>
2026-09-18 02:33:40 +0200

    M5.8: reset machine input controller

--- emulator/src/machine.c
+++ emulator/src/machine.c
@@ -1,7 +1,7 @@
 #include "k16/machine.h"
 int k16_machine_init(k16_machine_t *m,size_t n){m->ticks=0;return k16_memory_init(&m->memory,n);}
 void k16_machine_destroy(k16_machine_t *m){k16_memory_destroy(&m->memory);}
-void k16_machine_reset(k16_machine_t *m){m->ticks=0;k16_vpu_reset(&m->vpu);k16_copper_reset(&m->copper);k16_dma_reset(&m->dma);k16_cpu_reset(&m->cpu,&m->memory);}
+void k16_machine_reset(k16_machine_t *m){m->ticks=0;k16_vpu_reset(&m->vpu);k16_copper_reset(&m->copper);k16_dma_reset(&m->dma);k16_input_reset(&m->input,&m->memory);k16_cpu_reset(&m->cpu,&m->memory);}
 void k16_machine_start_copper(k16_machine_t *m,uint32_t a){k16_copper_start(&m->copper,a);}
 static void custom_tick(k16_machine_t *m){
  uint8_t display=(m->vpu.x<320u && m->vpu.y<200u)?1u:0u;
