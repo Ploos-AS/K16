3ad90897b424bdd5a1010d0e8cc13754f42304f5
pos <pgousdal@users.noreply.github.com>
2026-09-18 02:25:28 +0200

    M5.7: integrate Copper and DMA into machine

--- emulator/include/k16/machine.h
+++ emulator/include/k16/machine.h
@@ -3,9 +3,12 @@
 #include "k16/cpu.h"
 #include "k16/memory.h"
 #include "k16/vpu.h"
-typedef struct {k16_memory_t memory;k16_cpu_t cpu;k16_vpu_t vpu;uint64_t ticks;} k16_machine_t;
+#include "k16/copper.h"
+#include "k16/dma.h"
+typedef struct {k16_memory_t memory;k16_cpu_t cpu;k16_vpu_t vpu;k16_copper_t copper;k16_dma_t dma;uint64_t ticks;} k16_machine_t;
 int k16_machine_init(k16_machine_t *m,size_t ram_size);
 void k16_machine_destroy(k16_machine_t *m);
 void k16_machine_reset(k16_machine_t *m);
+void k16_machine_start_copper(k16_machine_t *m,uint32_t address);
 uint32_t k16_machine_step(k16_machine_t *m);
 #endif
