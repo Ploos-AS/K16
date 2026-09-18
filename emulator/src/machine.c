b75e20d945ac7acd80f4dab0f4600982eb35cbb0
pos <pgousdal@users.noreply.github.com>
2026-09-18 02:25:30 +0200

    M5.7: clock VPU Copper and DMA together

--- emulator/src/machine.c
+++ emulator/src/machine.c
@@ -1,5 +1,16 @@
 #include "k16/machine.h"
 int k16_machine_init(k16_machine_t *m,size_t n){m->ticks=0;return k16_memory_init(&m->memory,n);}
 void k16_machine_destroy(k16_machine_t *m){k16_memory_destroy(&m->memory);}
-void k16_machine_reset(k16_machine_t *m){m->ticks=0;k16_vpu_reset(&m->vpu);k16_cpu_reset(&m->cpu,&m->memory);}
-uint32_t k16_machine_step(k16_machine_t *m){uint32_t t=k16_cpu_step(&m->cpu,&m->memory);if(t){k16_vpu_step(&m->vpu,&m->memory,&m->cpu,t);m->ticks+=t;}return t;}
+void k16_machine_reset(k16_machine_t *m){m->ticks=0;k16_vpu_reset(&m->vpu);k16_copper_reset(&m->copper);k16_dma_reset(&m->dma);k16_cpu_reset(&m->cpu,&m->memory);}
+void k16_machine_start_copper(k16_machine_t *m,uint32_t a){k16_copper_start(&m->copper,a);}
+static void custom_tick(k16_machine_t *m){
+ uint8_t display=(m->vpu.x<320u && m->vpu.y<200u)?1u:0u;
+ uint8_t copper=k16_copper_wants_slot(&m->copper,m->vpu.x,m->vpu.y);
+ k16_dma_owner_t owner=k16_dma_grant(&m->dma,display,0,copper,0,0,0);
+ if(owner==K16_DMA_COPPER)k16_copper_step(&m->copper,&m->memory,m->vpu.x,m->vpu.y);
+ k16_vpu_step(&m->vpu,&m->memory,&m->cpu,1);
+}
+uint32_t k16_machine_step(k16_machine_t *m){
+ uint32_t n,t=k16_cpu_step(&m->cpu,&m->memory);if(!t)return 0;
+ for(n=0;n<t;n++)custom_tick(m);m->ticks+=t;return t;
+}
