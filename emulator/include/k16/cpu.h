2e5228352a89b772cb13b1a2529563f4e57676e3
pos <pgousdal@users.noreply.github.com>
2026-09-18 02:06:42 +0200

    M5.2: expand W65C816 execution semantics

--- emulator/include/k16/cpu.h
+++ emulator/include/k16/cpu.h
@@ -2,6 +2,14 @@
 #define K16_CPU_H
 #include <stdint.h>
 #include "k16/memory.h"
+#define K16_P_C 0x01u
+#define K16_P_Z 0x02u
+#define K16_P_I 0x04u
+#define K16_P_D 0x08u
+#define K16_P_X 0x10u
+#define K16_P_M 0x20u
+#define K16_P_V 0x40u
+#define K16_P_N 0x80u
 typedef struct {
     uint16_t a, x, y, sp, pc;
     uint8_t p, dbr, pbr;
