321904224fb430e5347f2bb5f6aee97aa78ee76e
pos <pgousdal@users.noreply.github.com>
2026-09-20 14:17:41 +0200

    M5.55: configure sparse checkout before corpus materialization

--- tools/fetch-processor-tests.sh
+++ tools/fetch-processor-tests.sh
@@ -6,11 +6,11 @@ set -eu
 DEST="${1:-.cache/ProcessorTests}"
 REV="${PROCESSOR_TESTS_REV:-bb11756436da8fd16cce86aef63dc6725f48836f}"
 if [ ! -d "$DEST/.git" ]; then
-  git clone --filter=blob:none --no-checkout https://github.com/SingleStepTests/ProcessorTests.git "$DEST"
+  git clone --filter=blob:none --no-checkout --depth=1 https://github.com/SingleStepTests/ProcessorTests.git "$DEST"
 fi
-git -C "$DEST" fetch --depth=1 origin "$REV"
-git -C "$DEST" checkout --detach FETCH_HEAD
 git -C "$DEST" sparse-checkout init --cone
 git -C "$DEST" sparse-checkout set 65816
+git -C "$DEST" fetch --depth=1 origin "$REV"
+git -C "$DEST" checkout --detach FETCH_HEAD
 printf '%s\n' "ProcessorTests 65816 oracle ready at $DEST"
 git -C "$DEST" rev-parse HEAD
